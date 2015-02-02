ed507a6c634c796c6454b7957c89df7e20fb2b3c
marcin@woflo.io <marcin@woflo.io>
2015-02-02 14:23:06 +0800

    fixes copy/paste mistake

--- app/initializers/config.js
+++ app/initializers/config.js
@@ -5,13 +5,13 @@ export default {
   name: 'ember-idx-modal',
   initialize: function() {
     if (!Em.Config) {
-      Em.Config = Config.create()
+      Em.Config = Config.create();
     }
 
     var defaultConfig = Em.Config.getConfig('bs');
     if (!defaultConfig) {
       Em.Config.addConfig('bs');
-+     defaultConfig = Em.Config.getConfig('bs');
+      defaultConfig = Em.Config.getConfig('bs');
     }
 
     defaultConfig['modal'] = {
